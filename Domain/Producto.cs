67ff6d93aa0215d0a13b45f7e8b54090d272564d
arielreyesflores <ariel.reyes.flores@gmail.com>
2020-09-01 12:22:49 -0600

    Update Producto.cs

--- Domain/Producto.cs
+++ Domain/Producto.cs
@@ -4,10 +4,11 @@ using System.Text;
 
 namespace Domain
 {
-    class Producto
+    public class Producto
     {
         public int ProductoId{get;set;}
         public string NombreProducto{get;set;}
- 
+        public List<Category> Categorias{get;set;}
+        public Marca MarcaProducto{get;set;}
     }
 }
